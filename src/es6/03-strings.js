14a2f73ac307579b9181498305a18cde56179d8d
Guillermo Quiñones <console.writeline@hotmail.com>
2022-11-23 19:01:40 -0600

    Strings

---
+++ src/es6/03-strings.js
@@ -0,0 +1,16 @@
+let hello = 'hello';
+let world = 'world';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+//template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//multi-line string
+let lorem='esto es un string\n'+'esto es otra llamda';
+console.log(lorem);
+
+let lorem2=`Esta es una frase épica
+la continuación de esa frase épica`;
+console.log(lorem2);
