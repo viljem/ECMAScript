15ddcdf7315f745b0548d39ae3dc60642869ca10
Guillermo Quiñones <console.writeline@hotmail.com>
2022-12-12 14:49:46 -0600

    ES8: object entries y object values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,11 @@
+const countries ={
+    MX:'México',
+    CO:'Colombia',
+    CL:'Chile',
+    PE:'Peru'
+}
+
+console.log(Object.keys(countries));
+console.log(Object.values(countries));
+console.log(Object.entries(countries));
+
